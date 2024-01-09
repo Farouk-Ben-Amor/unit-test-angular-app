0fd7aaed3d2833e349ad79d45812827ca95d30d4
Farouk-Ben-Amor <faaroukbnamor@gmail.com>
2024-01-09 16:00:02 +0100

    testing demo done for app comp and user service

---
+++ src/app/user/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  users = [
+    {id : 1, name : 'Farouk'},
+    {id : 2, name : 'Amor'}
+  ]
+
+  constructor() { }
+  
+  getUsers(){
+    return of(this.users)
+  }
+}
